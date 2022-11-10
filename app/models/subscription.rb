c97b94bb48677d8fa5ec47e81d63b5fa22320cac
Jared Hardinger <jaredhardinger@gmail.com>
2022-11-09 21:34:36 -0500

    Create model tests and models

---
+++ app/models/subscription.rb
@@ -0,0 +1,10 @@
+class Subscription < ApplicationRecord
+  belongs_to :customer
+  has_many :subscription_teas
+  has_many :teas, through: :subscription_teas
+  
+  validates_presence_of :title,
+                        :price, 
+                        :status,
+                        :frequency
+end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   describe 'relationships' do
     it { should belong_to(:customer) }
+    it { should have_many(:subscription_teas) }
     it { should have_many(:teas) }
   end
 
