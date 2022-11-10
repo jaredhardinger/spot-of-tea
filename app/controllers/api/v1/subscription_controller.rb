8a0dfc53103827d84e6a78bf0dac79773c1b3d65
Jared Hardinger <jaredhardinger@gmail.com>
2022-11-10 10:25:23 -0500

    Add sad path and edit controller for sad path logic

--- app/controllers/api/v1/subscription_controller.rb
+++ app/controllers/api/v1/subscription_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::SubscriptionController < ApplicationController
   def index
-    customer = Customer.find(params[:customer_id])
-    subscriptions = customer.subscriptions
-    render json: SubscriptionSerializer.new(subscriptions), status: 200
+    customer = Customer.find_by(id: params[:customer_id])
+    if customer
+      subscriptions = customer.subscriptions
+      render json: SubscriptionSerializer.new(subscriptions), status: 200
+    else
+      render json: { status: "Not Found", code: 404, message: "Customer not found" }, status: 404
+    end
   end
 
   def create
--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -12,27 +12,21 @@ RSpec.describe "Subscription Request" do
   context '#Index' do 
     describe 'Happy Path' do 
       it 'Receives a request to see all subscriptions and returns a status 200 and a response' do 
-
-      # binding.pry
-
         get "/api/v1/subscription?customer_id=#{@customer.id}"
         expect(response).to have_http_status 200
-      #   parsed = JSON.parse(response.body, symbolize_names: true)
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        data = parsed[:data]
+        expect(data).to be_an(Array)
+        expect(data.count).to eq(5)
+        attributes = data[0][:attributes]
+        expect(attributes.count).to eq(4)
+      end
+    end
 
-      #   data = parsed[:data]
-      #   expect(data[:id]).to eq("null")
-      #   expect(data[:type]).to eq("books")
-      #   attributes = data[:attributes]
-      #   expect(attributes[:destination]).to eq("manistee")
-      #   expect(attributes[:total_books_found]).to eq(43)
-      #   forecast = attributes[:forecast]
-      #   expect(forecast[:summary]).to eq("overcast clouds")
-      #   expect(forecast[:temperature]).to eq("57.47 F")
-      #   books = attributes[:books]
-      #   expect(books[0][:isbn]).to be_an(Array)
-      #   expect(books[0][:title]).to be_a(String)
-      #   expect(books[0][:publisher]).to be_an(Array)
-      # end
+    describe 'Sad Path' do
+      it 'Receives a request with an invalid customer id and returns a status 404' do
+        get "/api/v1/subscription?customer_id=5435"
+        expect(response).to have_http_status 404
       end
     end
   end
